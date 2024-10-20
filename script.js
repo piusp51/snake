c38f24545dbe25b73e72bfb2b3e419f9b318fb4a
piusp51 <okosipiusp51@gmail.com>
2024-10-20 22:59:53 +0100

    basic canvas setup

---
+++ script.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", pageLoaded, false);
+function pageLoaded() {
+  canvasApp();
+}
+
+function canvasApp() {
+  const cvs = document.querySelector("canvas");
+  const ctx = cvs.getContext("2d");
+  const block = 20; //parts of snake
+
+  //width && height of canvas
+  const width = cvs.width;
+  const height = cvs.height;
+
+  //function calls
+  draw();
+
+  function draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, width, height);
+    grid('#eee');
+  }
+
+  function grid(color) {
+    ctx.fillStyle = color;
+    for (let y = 0; y < height / block; ++y) {
+      for (let x = 0; x < width / block; ++x) {
+        ctx.strokeRect(x * block, y * block, block, block);
+      }
+    }
+  }
+}
